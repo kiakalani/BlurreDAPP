b453cb95827fadae27a72c60250921d34ea0fac3
anoymous <kiakalani@cmail.carleton.ca>
2024-02-13 18:11:03 -0500

    Added the base python code

--- API/__init__.py
+++ API/__init__.py
@@ -2,11 +2,21 @@ from flask import Flask
 
 app = Flask('API', static_folder='static')
 
+def get_app() -> Flask:
+    """
+    A function to setup the necessary configurations
+    for the flask application and return the instance.
+    :return: The flask application.
+    """
 
+    app = Flask('API', static_folder='static')
+    with app.app_context():
+        pass
+    return app
 app.route('/api')
 def handle_api():
     return ''
 
 
 if __name__ == '__main__':
-    app.run()
+    get_app().run()
