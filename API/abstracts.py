b89d54bf5bcdc480448669786704e1cd86daea9e
anoymous <kiakalani@cmail.carleton.ca>
2024-02-15 16:24:55 -0500

    Finished abstraction for blueprints

--- API/abstracts.py
+++ API/abstracts.py
@@ -15,64 +15,38 @@ class BP:
         """
         self.__name = name
         self.__bp = Blueprint(name, __name__, url_prefix=f'/{name}')
-    
-    def register_method(self, func) -> None:
-        if not hasattr(func, '__call__'):
-            return
-        # print(func.__name__)
-        # Starts with get or post and ends with the route
-        pattern = re.compile(r'^bp_(?P<method>[^_]+)_?(?P<route>[^_]?\S*)$')
-        # Name of the function
-        fname = func.__name__
-        # Making sure the function is supposed to be for routing
-        m = pattern.match(fname)
-        if not m:
-            return
-        m = m.groupdict()
-        m['method'] = m['method'].upper()
-        if m['method'] not in [
-            'GET', 'POST', 'PUT', 'HEAD', 'DELETE',
-            'PATCH', 'OPTIONS', 'CONNECT', 'TRACE'
-        ]:
-            return
-        
-        route, method = m['route'], m['method']
-        f_inspect = inspect.getfullargspec(func)
-        the_route = f'/{route}'
-
-        # If return type is a list, this would mean the function is overloaded.
-        # Therefore, multiple functions would need to be added.
-        if f_inspect.annotations.get('return') == list:
-            for f in func():
-                tr = the_route
-                args = inspect.getfullargspec(f).args
-                # Creating the url for each function
-                for i in range(0, len(args)):
-                    if tr == '/':
-                        tr += '<' + args[i] + '>'
-                    else:
-                        tr += '/<' + args[i] + '>'
-                # Adding the route
-                self.__bp.add_url_rule(tr, tr, view_func=f, methods=[method])
-        else:
-            self.__bp.add_url_rule(the_route, the_route, view_func=func, methods=[method])
 
     @staticmethod
     @property
     def db(self) -> db.DBComps:
+        """
+        Getter method for accessing the database.
+        :return: The database components of the application
+        """
         db_comps: db.DBComps = current_app.config['DB']
         return db_comps
 
-    def group_functions(self):
+    def group_functions(self) -> dict:
+        """
+        This method groups all of the functions by their method and name of the
+        operation.
+        :return: A dictionary with the following structure:
+        {Method: {RouteName: [Functions]}}
+        """
+
         funcs = {}
         pattern = re.compile(r'^bp_(?P<method>[^_]+)_?(?P<route>[^_]?\S*)$')
+
         for func in dir(self):
             the_func = getattr(self, func)
+
             if hasattr(the_func, '__call__'):
                 m = pattern.match(func)
+
                 if m:
                     m = m.groupdict()
                     method, route = m.get('method'), m.get('route')
+                    # making sure the method is valid
                     if method.upper() not in [
                         'GET', 'POST', 'PUT', 'HEAD', 'DELETE',
                         'PATCH', 'OPTIONS', 'CONNECT', 'TRACE'
@@ -81,16 +55,46 @@ class BP:
                     if not funcs.get(method):
                         funcs[method] = {}
                     if not funcs[method].get(route):
-                        funcs[method][route] = [the_func] if inspect.getfullargspec(the_func).annotations.get('return') != list else the_func()
+                        funcs[method][route] = [the_func] \
+                            if inspect.getfullargspec(the_func).annotations.get('return') != list \
+                                else the_func()
         return funcs
 
-    def assemble_functions(self):
+    def create_tmp_func(self, cmps):
+        """
+        Creates the function that combines all of the methods
+        for the given route.
+        :param: cmps: The components of the route.
+        :return: A combined function that contains all the
+        operations for different methods.
+        """
+
+        def ret_func(*args, **kwargs):
+            for i in range(len(cmps['functions'])):
+                if request.method == cmps['method'][i]:
+                    return cmps['functions'][i](*args, **kwargs)
+            return 'Not Implemented', 400
+
+        return ret_func
+        
+
+    def assemble_functions(self) -> dict:
+        """
+        A method to assemble all of the functions with different
+        methods together.
+        :return: A dictionary containing all of the assembled
+        functions.
+        """
+
         funcs = self.group_functions()
+
         final_funcs = {}
+
         for method, functions  in funcs.items():
             method = method.upper()
             for fname in functions:
-
+                
+                # Supplying the information about the functions
                 for f in functions[fname]:
                     if not final_funcs.get(fname):
                         final_funcs[fname] = {}
@@ -103,16 +107,10 @@ class BP:
                         final_funcs[fname][num_args]['method'].append(method)
                         final_funcs[fname][num_args]['functions'].append(f)
                 
+                # Setting up the combiend functions for the operations.
                 for n, cmps in final_funcs[fname].items():
-                    def cmbd_func(*args, **kwargs):
-                        for i in range(len(cmps['functions'])):
-                            if request.method == cmps['method'][i]:
-                                return cmps['functions'][i](*args, **kwargs)
-                        
-                    final_funcs[fname][n]['function'] = cmbd_func
-        import pprint; pprint.pprint(final_funcs)
-                    
-                            
+                    cmbd_func = self.create_tmp_func(cmps)
+                    final_funcs[fname][n]['function'] = cmbd_func    
                     
 
         return final_funcs
@@ -120,7 +118,12 @@ class BP:
 
 
 
-    def register_all(self):
+    def register_all(self) -> None:
+        """
+        A function to register the blueprint and all of the
+        corresponding functions for routes.
+        :return: None
+        """
         funcs_to_register = self.assemble_functions()
         print(funcs_to_register)
         for route, contents in funcs_to_register.items():
@@ -130,7 +133,7 @@ class BP:
             for nparams, comps in contents.items():
                 t_rt = route
                 for i in range(nparams):
-                    t_rt += f'<arg{i}>/'
+                    t_rt += f'<a{i}>/'
                 print(comps)
                 print(t_rt)
                 self.__bp.add_url_rule(
--- API/auth.py
+++ API/auth.py
@@ -11,21 +11,20 @@ class Authorization(abstracts.BP):
     def bp_get():
         return 'Hello!'
 
-    #/auth/signin/p/q
     @staticmethod
     def bp_get_signin() -> list:
         def no_param():
             return 'One param'
-        def one_param(p):
-            return 'Param' + p
-        def two_param(p, q):
-            return 'Two Param' + p + ' ' + q
+        def one_param(a0):
+            return 'Param' + a0
+        def two_param(a0, a1):
+            return 'Two Param' + a1 + ' ' + a0
         return [one_param, two_param, no_param]
 
     @staticmethod
     def bp_post_signin() -> list:
-        def one_param(p):
-            return 'Param' + p
-        def two_param(p, q):
-            return 'Two Param' + p + ' ' + q
+        def one_param(a0):
+            return 'Post Param' + a0
+        def two_param(a0, a1):
+            return 'Post Two Param' + a0 + ' ' + a1
         return [one_param, two_param]
