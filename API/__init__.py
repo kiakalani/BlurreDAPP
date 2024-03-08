aced4a208e26ed74112a08ffdef04126a94cb013
anonymous <kalkia2000@gmail.com>
2024-03-08 14:48:31 -0500

    Added comments

--- API/__init__.py
+++ API/__init__.py
@@ -4,14 +4,21 @@ from flask import Flask, request
 from flask_login import current_user
 from flask_cors import CORS
 from flask_socketio import SocketIO
-
 import db
 import blueprints
 
 
     
 class FlaskApp:
-    def __init__(self):
+    """
+    A class that is responsible for assembling the
+    application together.
+    """
+    def __init__(self) -> None:
+        """
+        Constructor
+        :return: None
+        """
         self.__app = Flask('API', static_folder='static')
         self.__setup_config()
         self.__setup_socketio()
@@ -19,6 +26,10 @@ class FlaskApp:
             self.__load_blueprints()
 
     def __setup_config(self) -> None:
+        """
+        Sets up the initial configurations for the app.
+        :return: None
+        """
         self.__app.config['DB'] = db.init_db()
         self.__app.config['DEBUG'] = False
         self.__app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
@@ -28,7 +39,12 @@ class FlaskApp:
             if app.config['DB'].get('destroy'):
                 app.config['DB']['destroy']()
 
-    def __setup_socketio(self):
+    def __setup_socketio(self) -> None:
+        """
+        Sets up the socket io within the application.
+        :return: None
+        """
+
         socket = SocketIO(self.__app)
         self.__app.config['SOCKETIO'] = {'IO':socket, 'SIDS': {}}
         @socket.on('connect')
@@ -47,6 +63,12 @@ class FlaskApp:
 
 
     def __load_blueprints(self) -> None:
+        """
+        Loads the blueprints automatically into the
+        flask application.
+        :return: None
+        """
+
         import blueprints.auth as auth
         
 
@@ -58,9 +80,27 @@ class FlaskApp:
 
     @property 
     def socket(self) -> SocketIO:
-        return self.__app.config['SOCKETIO']['IO']
+        """
+        Returns the socket instance of the application
+        :return: The socket instance of app.
+        """
 
-    def run(self):
+        return self.__app.config['SOCKETIO']['IO']
+    
+    @property
+    def db(self) -> db.DBComps:
+        """
+        Provides the db components.
+        :return: DBComps of the applications
+        """
+
+        return self.__app.config['DB']
+
+    def run(self) -> None:
+        """
+        Runs the application.
+        :return: None
+        """
         CORS(self.__app, supports_credentials=True, origins='*')
         self.socket.run(self.__app, debug=self.__app.config['DEBUG'], host='0.0.0.0', port=3001)
 
@@ -68,6 +108,11 @@ class FlaskApp:
 __flask_app = FlaskApp()
 
 def get_flask_app():
+    """
+    Getter for the flask app instance.
+    :return: The flask app instance
+    """
+
     return __flask_app
             
 if __name__ == '__main__':
